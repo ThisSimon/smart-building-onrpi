65c0a863c4cc2db56efd4a4239a2eb33ee4093bb
ThisSimon <c00208558@itcarlow.ie>
2019-04-15 09:26:55 +0100

    initial commit including node-red

---
+++ installProject.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade -y
+
+#sudo apt-get install git -y
+
+#sudo apt-get install build-essential -y
+
+## To install node-red use following command
+#bash <(curl -sL https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/update-nodejs-and-nodered)
+## To run as service use this command
+# sudo systemctl enable nodered.service
+
+# To stop the service use
+# sudo systemctl disable nodered.service
